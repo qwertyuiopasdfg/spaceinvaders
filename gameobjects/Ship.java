27e30cea8c6ea50005f52b72e610e5702612443a
DrMastodont <antoha.turin@gmail.com>
2024-02-29 21:25:00 +0700

    Добавление корабля игрока

--- gameobjects/Ship.java
+++ gameobjects/Ship.java
@@ -1,6 +1,8 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
 public class Ship extends GameObject {
+
+    public boolean isAlive = true;
     public Ship (double x, double y) {
         super(x, y);
     }
@@ -9,5 +11,8 @@ public class Ship extends GameObject {
     }
     public Bullet fire () {
         return null;
+    }
+    public void kill() {
+        isAlive = false;
     }
 }
